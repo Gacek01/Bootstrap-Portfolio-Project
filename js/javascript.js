7ccfdc569be8f17ef871af98c5c29826fcb9b018
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-19 12:40:28 +0100

    init

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+'use strict';
+$(function () {
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		$('body').animate({
+			scrollTop: $(this.hash).offset().top - 50
+		}, 500);
+	});
+	//Efekt Nav-Bar Transparency
+	$(window).scroll(function () {
+			if ($(window).scrollTop() > 71) {
+				$('#main-nav').css({
+					"background-color": "aquamarine"
+					, "transition-duration": "1s"
+				})
+			}
+			else {
+				$('#main-nav').css({
+					"background-color": "transparent"
+					, "transition-duration": "1s"
+				})
+			}
+		})
+		//Efekt Smooth Scroll
+	window.sr = ScrollReveal().reveal("[class*=col-], .row, row>h2");
+	sr.reveal('figure', {
+		duration: 2000
+	}, 300);
+})
